67c5492df7e577d990b347f4f33ba3a5386b86cb
ricardo-2 <ricardo-2ricardomoralesricardo-2@hotmail.com>
2022-02-03 22:50:06 -0600

    Segundo Commit

---
+++ src/components/Cotizacion.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+const ResultadoDiv = styled.div`
+    color: #FFF;
+    font-family: Arial, Helvetica, sans-serif;
+`;
+
+const Info = styled.p`
+    font-size: 18px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+const Precio = styled.p`
+   font-size: 30px;
+`;
+
+const Cotizacion = ({resultado}) => {
+    if(Object.keys(resultado).length === 0) return null;
+
+    console.log(resultado);
+
+    return ( 
+        <ResultadoDiv>
+            <Precio>El precio es: <span>{resultado.PRICE}</span></Precio>
+            <Info>Precio mas alto del dia: <span>{resultado.HIGHDAY}</span></Info>
+            <Info>Precio mas bajo del dia: <span>{resultado.LOWDAY}</span></Info>
+            <Info>Variacion ultimas 24 horas: <span>{resultado.CHANGEPCT24HOUR}</span></Info>
+            <Info>Ultima actualizacion: <span>{resultado.LASTUPDATE}</span></Info>
+        </ResultadoDiv>
+     );
+}
+ 
+export default Cotizacion;
